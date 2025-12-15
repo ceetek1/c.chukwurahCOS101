349386f1153fc4f9c13df5224a43f74386209790
ceetek1 <charles.chukwurah@pau.edu.ng>
2025-12-15 17:34:28 +0100

    updated it properly

---
+++ Week_8/practice_6/target/debug/deps/practice_6-b7b3632336f092e8.d
@@ -0,0 +1,5 @@
+/Users/chukwurahcharles/Desktop/c.chukwurahCOS101 /Week_8/practice_6/target/debug/deps/practice_6-b7b3632336f092e8.d: src/main.rs
+
+/Users/chukwurahcharles/Desktop/c.chukwurahCOS101 /Week_8/practice_6/target/debug/deps/practice_6-b7b3632336f092e8: src/main.rs
+
+src/main.rs:
---
+++ Week_8/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+/Users/chukwurahcharles/Desktop/c.chukwurahCOS101\ /Week_8/practice_6/target/debug/practice_6: /Users/chukwurahcharles/Desktop/c.chukwurahCOS101\ /Week_8/practice_6/src/main.rs
