5dad8a08276f681236ae178fe5ae4156770418d1
ceetek1 <charles.chukwurah@pau.edu.ng>
2025-12-15 19:17:46 +0100

    Rust slice

---
+++ Week_8/practice_11/target/debug/deps/practice_11-c802a64f138902dd.d
@@ -0,0 +1,5 @@
+/Users/chukwurahcharles/Desktop/c.chukwurahCOS101 /Week_8/practice_11/target/debug/deps/practice_11-c802a64f138902dd.d: src/main.rs
+
+/Users/chukwurahcharles/Desktop/c.chukwurahCOS101 /Week_8/practice_11/target/debug/deps/practice_11-c802a64f138902dd: src/main.rs
+
+src/main.rs:
---
+++ Week_8/practice_11/target/debug/practice_11.d
@@ -0,0 +1,1 @@
+/Users/chukwurahcharles/Desktop/c.chukwurahCOS101\ /Week_8/practice_11/target/debug/practice_11: /Users/chukwurahcharles/Desktop/c.chukwurahCOS101\ /Week_8/practice_11/src/main.rs
