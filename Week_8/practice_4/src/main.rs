18ca35e19dbaf69c60245f59c3c51f261e0564b1
ceetek1 <charles.chukwurah@pau.edu.ng>
2025-12-15 17:36:20 +0100

    updated it properly

--- Week_8/practice_4/src/main.rs
+++ Week_8/practice_4/src/main.rs
@@ -1,9 +1,9 @@
 fn main (){
     // Name Vector 
-    let name = vec!["mary","sam","sally","greg","Ade","mark","june","ife"];fn
+    let name = vec!["mary","sam","sally","greg","Ade","mark","june","ife"];
     // Age vector
-    let age = vec![16,17,19,22,21,18,23];fn
-    println!("\n Age allocation:\n");fn
+    let age = vec![16,17,19,22,21,18,23];
+    println!("\n Age allocation:\n");
     // loop to iterate elements in vector
     for i in 0..age.len(){
         // iterating through i on the vector
