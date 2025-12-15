b8155caffa89fbce8b10bae85fcff6f21d5a3bd0
ceetek1 <charles.chukwurah@pau.edu.ng>
2025-12-15 19:28:33 +0100

    Destructuring a tuple

---
+++ Week_8/practice_10/target/debug/deps/practice_10-c7a1683bccf7adc9.d
@@ -0,0 +1,5 @@
+/Users/chukwurahcharles/Desktop/c.chukwurahCOS101 /Week_8/practice_10/target/debug/deps/practice_10-c7a1683bccf7adc9.d: src/main.rs
+
+/Users/chukwurahcharles/Desktop/c.chukwurahCOS101 /Week_8/practice_10/target/debug/deps/practice_10-c7a1683bccf7adc9: src/main.rs
+
+src/main.rs:
---
+++ Week_8/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+/Users/chukwurahcharles/Desktop/c.chukwurahCOS101\ /Week_8/practice_10/target/debug/practice_10: /Users/chukwurahcharles/Desktop/c.chukwurahCOS101\ /Week_8/practice_10/src/main.rs
